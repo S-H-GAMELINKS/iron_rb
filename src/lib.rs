2652b0349a831e890dd7a4d2e5f3a30bb5eb94b3
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-05-17 16:09:45 +0900

    run server

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+extern crate iron;
+use iron::prelude::*;
+use iron::status;
+
 #[macro_use]
 extern crate helix;
 
 ruby! {
-
+    class IronRb {
+        def run() {
+            Iron::new(|_: &mut Request| {
+                Ok(Response::with((status::Ok, "Hello world!")))
+            }).http("localhost:3000");
+        }
+    }
 }
